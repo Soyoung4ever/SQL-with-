b595454e7df10f33275466874d4942059b63008c
Soyoung4ever <salang052@gmail.com>
2024-08-14 10:24:48 +0900

    Update Week 1.sql

--- Case_Study/Week 1.sql
+++ Case_Study/Week 1.sql
@@ -1,17 +1,3 @@
-/*
-1. **Basic Data Exploration**
-    - Display the first names, last names, and emails of all customers.
-    - Display the names and join dates of customers who joined in 2024.
-2. **Sales Data Analysis**
-    - Display the order ID, order date, and total amount for all orders.
-    - Display orders with a total amount of $100 or more.
-3. **Product Data Exploration**
-    - Display the names and prices of all products.
-    - Display products with a price of $50 or more.
-4. **Filtering Data by Specific Conditions**
-    - Display the names and prices of products in the "Electronics" category.
-    - Display the order ID, order date, and total amount for orders placed after "2024-01-01".
-*/
 -- Customers table
 CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
@@ -78,3 +64,21 @@ VALUES
 (102, 1002, 1, 1200.00),
 (103, 1003, 2, 150.00),
 (103, 1004, 5, 20.00);
+
+/*
+1. **Basic Data Exploration**
+    - Display the first names, last names, and emails of all customers.
+    - Display the names and join dates of customers who joined in 2024.
+2. **Sales Data Analysis**
+    - Display the order ID, order date, and total amount for all orders.
+    - Display orders with a total amount of $100 or more.
+3. **Product Data Exploration**
+    - Display the names and prices of all products.
+    - Display products with a price of $50 or more.
+4. **Filtering Data by Specific Conditions**
+    - Display the names and prices of products in the "Electronics" category.
+    - Display the order ID, order date, and total amount for orders placed after "2024-01-01".
+*/
+--Answer 1-1
+select first_name, last_name, email
+from *
